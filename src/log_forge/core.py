b92482d75d1691a93f915fcf0e64e61e9c103e08
Andrew Griffith <agriffi10@gmail.com>
2026-06-19 11:50:29 -0400

    adding in initial setup files

---
+++ src/log_forge/core.py
@@ -0,0 +1,13 @@
+from log_forge.modules.v1.log import Log
+
+class LogForge():
+
+    def __init__(self):
+        self.called = True
+    
+
+    def configure_logs(self):
+        return
+
+    def forge_log(self):
+        return Log()
